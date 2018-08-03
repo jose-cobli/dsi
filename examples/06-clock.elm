20f7d6257481d0c11b43b9374c33979051fb7eea
José Rodrigues <jose@cobli.co>
2018-08-03 11:34:06 -0300

    Add relative position and a blue square

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -1,12 +1,14 @@
-import Html exposing (Html, div)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import Html exposing (Html, div, p, text)
+import Html.Attributes exposing (style)
+-- import Svg exposing (..)
+-- import Svg.Attributes exposing (..)
+import Css exposing (..)
 import Time exposing (Time, second)
 import Mouse 
 import Keyboard
 
 
-
+main : Program Never Model Msg
 main =
   Html.program
     { init = init
@@ -20,12 +22,21 @@ main =
 -- MODEL
 
 
-type alias Model = Time
-
+type alias Model = 
+  { time: Time
+  , x: Int
+  , y: Int
+  }
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  (
+    { time = 0
+    , x = 0
+    , y = 0
+    }
+    , Cmd.none
+   )
 
 
 
@@ -34,13 +45,16 @@ init =
 
 type Msg
   = Tick Time
+  | Position Int Int
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick newTime ->
-      (newTime, Cmd.none)
+      ( { model | time = newTime }, Cmd.none)
+    Position x y ->
+      ( { model | x = x, y = y }, Cmd.none)
 
 
 
@@ -49,7 +63,10 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+  Sub.batch
+    [ Time.every second Tick
+    , Mouse.moves (\{x, y} -> Position x y)
+    ]
 
 
 
@@ -59,19 +76,33 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   let
-    angle =
-      turns (Time.inMinutes model)
-
-    handX =
-      toString (50 + 40 * cos angle)
-
-    handY =
-      toString (50 + 40 * sin angle)
+    mstyle = 
+      style
+        [ ("position", "relative")
+        , ("backgroundColor", "blue")
+        , ("height", "100px")
+        , ("width", "100px")
+        , ("margin", "50px")
+        ]
   in
     div []
-      [ text (toString model)
-        , div []
-          [ text <| toString <| Basics.round <| Time.inSeconds model ]
-        , div []
-          [ text <| toString <| cos <| Time.inMinutes model ]
-      ]
+      [ div [ mstyle ] []
+      , p [] [ text <| "Model: " ++ toString model ]
+      , p [] [ text <| "rel pos: " ++ relpos model ]
+      , p [] [ text <| "inSeconds: " ++ (toString <| Basics.round <| Time.inSeconds model.time) ]
+    ]
+
+
+
+-- AUX
+
+
+relpos : Model -> String
+relpos model =
+  let
+    rp =
+      { x = model.x - 100
+      , y = model.y - 100
+      }
+  in
+    toString rp
