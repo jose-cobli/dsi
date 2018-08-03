e7b7ea2c519fba5546e8c2c7df1b4269e4de34d8
José Rodrigues <jose@cobli.co>
2018-08-03 10:49:46 -0300

    Remove clock, add texts and mouse/keyboard imports

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -1,7 +1,9 @@
-import Html exposing (Html)
+import Html exposing (Html, div)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
+import Mouse 
+import Keyboard
 
 
 
@@ -66,7 +68,10 @@ view model =
     handY =
       toString (50 + 40 * sin angle)
   in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+    div []
+      [ text (toString model)
+        , div []
+          [ text <| toString <| Basics.round <| Time.inSeconds model ]
+        , div []
+          [ text <| toString <| cos <| Time.inMinutes model ]
       ]
